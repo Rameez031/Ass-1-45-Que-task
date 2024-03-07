c22f4a92c77cbd8828ed76d92e8cf345b38c3375
Rameez <sheikhrameez031@gmail.com>
2024-03-08 02:32:22 +0500

    currently i create this directory and some packages files

---
+++ app.js
@@ -0,0 +1,4 @@
+"use strict";
+//Question 2
+let msg = "Hello Rameez, would you like to learn some Python today?!";
+console.log(msg);
---
+++ app.ts
@@ -0,0 +1,3 @@
+//Question 2
+let msg: string = "Hello Rameez, would you like to learn some Python today?!";
+console.log(msg);
