4adb27ed904bbcb336ead66902856886a9d3632c
Rameez <sheikhrameez031@gmail.com>
2024-03-08 03:05:08 +0500

    i have completed some task of this my first assignement

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-"use strict";
-//Question 2
-let msg = "Hello Rameez, would you like to learn some Python today?!";
+// Question 2
+var msg = "Hello Rameez, would you like to learn some Python today?!";
 console.log(msg);
+// Question 3
+// Convert into LowerCase:
+var personName = 'Elon Musk';
+console.log(personName.toLowerCase());
+// Convert into UpperCase:
+personName = 'Bill Gates';
+console.log(personName.toUpperCase());
+// Convert into TitleCase
+personName = 'Brenden erich';
+console.log(console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase()));
--- app.ts
+++ app.ts
@@ -1,3 +1,15 @@
-//Question 2
+// Question 2
 let msg: string = "Hello Rameez, would you like to learn some Python today?!";
 console.log(msg);
+
+// Question 3
+// Convert into LowerCase:
+let personName: string = 'Elon Musk';
+console.log(personName.toLowerCase());
+// Convert into UpperCase:
+personName = 'Bill Gates';
+console.log(personName.toUpperCase());
+// Convert into TitleCase
+personName = 'Brenden erich';
+console.log(console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase()));
+
